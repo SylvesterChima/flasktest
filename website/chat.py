bd61731e7f3af86d90cf302d9df5987e70f3368a
sylvester Eze <sylvesterchima11@outlook.com>
2023-03-15 20:13:30 +0100

    wip

--- website/chat.py
+++ website/chat.py
@@ -14,14 +14,28 @@ fb_access_token = os.getenv('FB_ACCESS_TOKEN')
 insta_access_token = os.getenv('INSTA_ACCESS_TOKEN')
 wp_access_token = os.getenv('WP_ACCESS_TOKEN')
 
-def is_json_key_present(json, key):
+def is_message_notification(data):
     try:
-        buf = json[key]
+        if data ["object"]=="page":  #if true hngeeb entry [list]
+            for entry in data["entry"]:
+                for messaging_event in entry["messaging"]: #3shan ad5ol 3la list 
+                    #sender_id = messaging_event["sender"]["id"]
+                    #recipient_id = messaging_event["recipient"]["id"] #bgeb mn eldict elrecipient key
+                    if messaging_event.get("message"):
+                        return True
+                    else:
+                        return False
+        elif data ["object"]=="whatsapp_business_account":
+            for entry in data["entry"]:
+                for changes_event in entry["changes"]: #3shan ad5ol 3la list 
+                    if changes_event["field"] == "messages":
+                        return True
+                    else:
+                        return False
+        else:
+            return False
     except KeyError:
         return False
-
-    return True
-
 
 # facebook messenger webhook
 @chat.route('/webhook', methods=['GET'])
@@ -34,23 +48,34 @@ def webhook_verify():
 def webhook_action():
     data = json.loads(request.data.decode('utf-8'))
     payload = request.get_data()
+    mjson = request.get_json()
     logging.error("****** payload ******")
     logging.error(payload)
     logging.error("****** end payload ******")
+    logging.error("****** mjson ******")
+    logging.error(mjson)
+    logging.error("****** end mjson ******")
     logging.error(data)
-    for entry in data['entry']:
-        user_message = entry['messaging'][0]['message']['text']
-        user_id = entry['messaging'][0]['sender']['id']
-        response = {
-            'recipient': {'id': user_id},
-            "messaging_type": "RESPONSE",
-            "message":{
-                "text":"Hello, world!"
-            }
-        }
-        response['message']['text'] = fb_handle_message(user_id, user_message)
-        r = requests.post('https://graph.facebook.com/v16.0/108409538867050/messages/?access_token=' + fb_access_token, json=response)
+    if is_message_notification(mjson):
+        for entry in mjson["entry"]:
+            for messaging_event in entry["messaging"]: #3shan ad5ol 3la list 
+                sender_id = messaging_event["sender"]["id"]
+                recipient_id = messaging_event["recipient"]["id"] #bgeb mn eldict elrecipient key
+                timestamp = messaging_event["timestamp"]
+
+                if "text" in messaging_event["message"]: #key:text 
+                    message_id = messaging_event["message"]["mid"]
+                    sender_message = messaging_event["message"]["text"] # message text ="hello there"
+                    response = {
+                        'recipient': {'id': sender_id},
+                        "messaging_type": "RESPONSE",
+                        "message":{
+                            "text": fb_handle_message(sender_id, sender_message)
+                        }
+                    }
+                    r = requests.post('https://graph.facebook.com/v16.0/108409538867050/messages/?access_token=' + fb_access_token, json=response)
     return Response(response="EVENT RECEIVED",status=200)
+
 
 def fb_handle_message(user_id, user_message):
     # DO SOMETHING with the user_message ... ¯\_(ツ)_/¯
@@ -96,16 +121,25 @@ def wp_webhook_verify():
 
 @chat.route('/whatsapp/webhook', methods=['POST'])
 def wp_webhook_action():
-    data = json.loads(request.data.decode('utf-8'))
-    logging.error(data)
-    json_data = {"messaging_product": "whatsapp","to": "2348036496516","type": "template",
-    "template": {
-        "name": "hello_world",
-        "language": {
-            "code": "en_US"
-        }
-    }}
-    response = requests.post('https://graph.facebook.com/v16.0/110958208603472/messages?access_token=' + insta_access_token, json=json_data)
+    mjson = request.get_json()
+    if is_message_notification(mjson):
+        for entry in mjson["entry"]:
+            for changes in entry["changes"]:
+                for messages in entry["value"]["messages"]:
+                    message_id = messages["id"]
+                    timestamp = messages["timestamp"]
+                    message_type = messages["type"]
+                    sender = messages["from"]
+                    sender_message = messages["text"]["body"]
+
+                    json_data = {"messaging_product": "whatsapp","to": "2348036496516","type": "template",
+                    "template": {
+                        "name": "hello_world",
+                        "language": {
+                            "code": "en_US"
+                        }
+                    }}
+                    response = requests.post('https://graph.facebook.com/v16.0/110958208603472/messages?access_token=' + wp_access_token, json=json_data)
     return Response(response="EVENT RECEIVED",status=200)
 
 def wp_handle_message(user_id, user_message):
